7392c71e81df130c0dba6327e46880d361b1f318
Sebastien hamel <sebastien.hamel@gmail.com>
2018-03-21 12:27:06 -0400

    NW-741: remove pending actions and closures and also condition in the Igloo to simplify everything.

--- Sources/Dispatcher.swift
+++ Sources/Dispatcher.swift
@@ -25,12 +25,10 @@ public protocol Dispatcher {
     /// also has to be run at each time. So the code to handle the completion should
     /// really be written in each action.
     @discardableResult
-    func dispatch<S: Store>(store: S, action: ActionType, condition: ((S) -> Bool)?, completion: ((S, ActionResult?) -> Promise<ActionResult?>)?) -> Promise<ActionResult?>
+    func dispatch<S: Store>(store: S, action: ActionType, completion: ((S, ActionResult?) -> Promise<ActionResult?>)?) -> Promise<ActionResult?>
     
     @discardableResult
-    func dispatch<S: Store>(store: S, closure: @escaping (S) -> Promise<ActionResult?>, condition: ((S) -> Bool)?, completion: ((S, ActionResult?) -> Promise<ActionResult?>)?) -> Promise<ActionResult?>
-    
-    func pendingTasks<S: Store>(store: S) -> (() -> Promise<Void>)
+    func dispatch<S: Store>(store: S, closure: @escaping (S) -> Promise<ActionResult?>, completion: ((S, ActionResult?) -> Promise<ActionResult?>)?) -> Promise<ActionResult?>
     
     func register<S: Store>(store: S)
 }
@@ -55,142 +53,39 @@ extension Dispatcher {
     /// also has to be run at each time. So the code to handle the completion should
     /// really be written in each action.
     @discardableResult
-    public func dispatch<S: Store>(store: S, action: ActionType, condition: ((S) -> Bool)? = nil, completion: ((S, ActionResult?) -> Promise<ActionResult?>)? = nil) -> Promise<ActionResult?> {
+    public func dispatch<S: Store>(store: S, action: ActionType, completion: ((S, ActionResult?) -> Promise<ActionResult?>)? = nil) -> Promise<ActionResult?> {
         
         return Promise<ActionResult?> { fulfill, reject in
-            
-            if let condition = condition {
                 
-                if condition(store) {
-                    
-                    firstly {
-                        // remove the handling of the completion it<s already handled here
-                        // because the pending task puts it inside the pending tasks, and execute
-                        // this task first./
-                        store.dispatch(action: action, condition: nil, completion: completion)
-                    }.then { actionResult -> Promise<ActionResult?> in
-                        return Promise<ActionResult?> { fulfill, reject in
-                            self.pendingTasks(store: store)().then {
-                                fulfill(actionResult)
-                            }.catch { error in
-                                reject(error)
-                            }
-                        }
-                    }.then { actionResult in
-                        fulfill(actionResult)
-                    }.catch { error in
-                        debugPrint("Error: \(error)")
-                        reject(error)
-                    }
-                }
-                else {
-                    // keep the pending task for later
-                    store.pendingStoreActions.append((condition, action, completion))
-                    fulfill(nil)
-                }
-            }
-            else {
-                
-                // if there is no condition we execute simply the closure, followed with
-                // the completion and finally all the pending tasks closures.
-                firstly {
-                    store.dispatch(action: action, condition: nil, completion: completion)
-                }.then { actionResult -> Promise<ActionResult?> in
-                    return Promise<ActionResult?> { fulfill, reject in
-                        self.pendingTasks(store: store)().then {
-                            fulfill(actionResult)
-                        }.catch { error in
-                            reject(error)
-                        }
-                    }
-                }.then { actionResult -> Void in
-                    fulfill(actionResult)
-                }.catch { error in
-                    debugPrint("Error: \(error)")
-                    reject(error)
-                }
+            // if there is no condition we execute simply the closure, followed with
+            // the completion and finally all the pending tasks closures.
+            firstly {
+                store.dispatch(action: action, completion: completion)
+            }.then { actionResult -> Void in
+                fulfill(actionResult)
+            }.catch { error in
+                debugPrint("Error: \(error)")
+                reject(error)
             }
         }
     }
     
     @discardableResult
-    public func dispatch<S: Store>(store: S, closure: @escaping (S) -> Promise<ActionResult?>, condition: ((S) -> Bool)?, completion: ((S, ActionResult?) -> Promise<ActionResult?>)? = nil) -> Promise<ActionResult?> {
+    public func dispatch<S: Store>(store: S, closure: @escaping (S) -> Promise<ActionResult?>, completion: ((S, ActionResult?) -> Promise<ActionResult?>)? = nil) -> Promise<ActionResult?> {
         
         return Promise<ActionResult?> { fulfill, reject in
             
-            if let condition = condition {
-                
-                if condition(store) {
-                    
-                    firstly {
-                        store.dispatch(closure: closure, condition: nil, completion: completion)
-                    }.then { actionResult -> Promise<ActionResult?> in
-                        return Promise<ActionResult?> { fulfill, reject in
-                            self.pendingTasks(store: store)().then {
-                                fulfill(actionResult)
-                            }.catch { error in
-                                reject(error)
-                            }
-                        }
-                    }.then { actionResult in
-                        fulfill(actionResult)
-                    }.catch { error in
-                        debugPrint("Error: \(error)")
-                        reject(error)
-                    }
-                }
-                else {
-                    // keep the pending task for later
-                    store.pendingStoreClosures.append((condition, closure, completion))
-                    fulfill(nil)
-                }
-            }
-            else {
-                
-                // if there is no condition we execute simply the closure, followed with
-                // the completion and finally all the pending tasks closures.
-                firstly {
-                    store.dispatch(closure: closure, condition: nil, completion: completion)
-                }.then { actionResult -> Promise<ActionResult?> in
-                    return Promise<ActionResult?> { fulfill, reject in
-                        self.pendingTasks(store: store)().then {
-                            fulfill(actionResult)
-                        }.catch { error in
-                            reject(error)
-                        }
-                    }
-                }.then { actionResult in
-                    fulfill(actionResult)
-                }.catch { error in
-                    debugPrint("Error: \(error)")
-                    reject(error)
-                }
+            // if there is no condition we execute simply the closure, followed with
+            // the completion and finally all the pending tasks closures.
+            firstly {
+                store.dispatch(closure: closure, completion: completion)
+            }.then { actionResult in
+                fulfill(actionResult)
+            }.catch { error in
+                debugPrint("Error: \(error)")
+                reject(error)
             }
         }
-    }
-    
-    public func pendingTasks<S: Store>(store: S) -> (() -> Promise<Void>) {
-        
-        // Default handling when there is not coordination
-        // between stores
-        let pendingTasksClosure = { () -> Promise<Void> in
-            
-            return Promise<Void> { fulfill, reject in
-                
-                firstly {
-                    store.executeExecutablePendingStoreClosures()
-                }.then {
-                    store.executeExecutablePendingStoreActions()
-                }.then {
-                    fulfill(())
-                }.catch { error in
-                    debugPrint("Error: \(error)")
-                    reject(error)
-                }
-            }
-        }
-        
-        return pendingTasksClosure
     }
     
     public func register<S: Store>(store: S) {
--- Sources/Store.swift
+++ Sources/Store.swift
@@ -15,23 +15,15 @@ public protocol Store: class {
     
     var reducer: ReducerType { get }
     
-    var pendingStoreClosures: [(((Self) -> Bool), (Self) -> Promise<ActionResult?>, ((Self, ActionResult?) -> Promise<ActionResult?>)?)] { get set }
-    
-    var pendingStoreActions: [(((Self) -> Bool), ActionType, ((Self, ActionResult?) -> Promise<ActionResult?>)?)] { get set }
-    
     @discardableResult
     func readSync<R>(in dispatchQueue: DispatchQueue, with closure: @escaping (Self) throws -> R) -> Promise<R>
     
     @discardableResult
     func readAsync<R>(in dispatchQueue: DispatchQueue, with closure: @escaping (Self) throws -> R) -> Promise<R>
     
-    func dispatch(closure: @escaping (Self) -> Promise<ActionResult?>, condition: ((Self) -> Bool)?, completion: ((Self, ActionResult?) -> Promise<ActionResult?>)?) -> Promise<ActionResult?>
+    func dispatch(closure: @escaping (Self) -> Promise<ActionResult?>, completion: ((Self, ActionResult?) -> Promise<ActionResult?>)?) -> Promise<ActionResult?>
     
-    func dispatch(action: ActionType, condition: ((Self) -> Bool)?, completion: ((Self, ActionResult?) -> Promise<ActionResult?>)?) -> Promise<ActionResult?>
-    
-    func executeExecutablePendingStoreClosures() -> Promise<Void>
-    
-    func executeExecutablePendingStoreActions() -> Promise<Void>
+    func dispatch(action: ActionType, completion: ((Self, ActionResult?) -> Promise<ActionResult?>)?) -> Promise<ActionResult?>
 }
 
 extension Store {
@@ -48,237 +40,50 @@ extension Store {
         return reducer.readAsync(store: self, in: dispatchQueue, with: closure)
     }
     
-    public func dispatch(closure: @escaping (Self) -> Promise<ActionResult?>, condition: ((Self) -> Bool)? = nil, completion: ((Self, ActionResult?) -> Promise<ActionResult?>)? = nil) -> Promise<ActionResult?> {
+    public func dispatch(closure: @escaping (Self) -> Promise<ActionResult?>, completion: ((Self, ActionResult?) -> Promise<ActionResult?>)? = nil) -> Promise<ActionResult?> {
         
         return Promise<ActionResult?> { fulfill, reject in
             
-            if let condition = condition {
+            if let completion = completion {
                 
-                if condition(self) {
-                    
-                    if let completion = completion {
-                        
-                        firstly {
-                            closure(self)
-                        }.then { closureResult -> Promise<ActionResult?> in
-                            completion(self, closureResult)
-                        }.then { completionResult -> Void in
-                            fulfill(completionResult)
-                        }.catch { error in
-                            debugPrint("Error: \(error)")
-                            reject(error)
-                        }
-                    }
-                    else {
-                        
-                        firstly {
-                            closure(self)
-                        }.then { closureResult -> Void in
-                            fulfill(closureResult)
-                        }.catch { error in
-                            debugPrint("Error: \(error)")
-                            reject(error)
-                        }
-                    }
-                    
-                }
-                else {
-                    pendingStoreClosures.append((condition, closure, completion))
-                    fulfill(nil)
+                firstly {
+                    closure(self)
+                }.then { closureResult -> Promise<ActionResult?> in
+                    completion(self, closureResult)
+                }.then { completionResult -> Void in
+                    fulfill(completionResult)
+                }.catch { error in
+                    debugPrint("Error: \(error)")
+                    reject(error)
                 }
             }
             else {
-                if let completion = completion {
-                    
-                    firstly {
-                        closure(self)
-                    }.then { closureResult -> Promise<ActionResult?> in
-                        completion(self, closureResult)
-                    }.then { completionResult -> Void in
-                        fulfill(completionResult)
-                    }.catch { error in
-                        debugPrint("Error: \(error)")
-                        reject(error)
-                    }
-                }
-                else {
-                    
-                    firstly {
-                        closure(self)
-                    }.then { closureResult -> Void in
-                        fulfill(closureResult)
-                    }.catch { error in
-                        debugPrint("Error: \(error)")
-                        reject(error)
-                    }
+                
+                firstly {
+                    closure(self)
+                }.then { closureResult -> Void in
+                    fulfill(closureResult)
+                }.catch { error in
+                    debugPrint("Error: \(error)")
+                    reject(error)
                 }
             }
         }
     }
     
     @discardableResult
-    public func dispatch(action: ActionType, condition: ((Self) -> Bool)? = nil, completion: ((Self, ActionResult?) -> Promise<ActionResult?>)? = nil) -> Promise<ActionResult?> {
+    public func dispatch(action: ActionType, completion: ((Self, ActionResult?) -> Promise<ActionResult?>)? = nil) -> Promise<ActionResult?> {
         
         return Promise<ActionResult?> { fulfill, reject in
-            
-            if let condition = condition {
                 
-                if condition(self) {
-                    
-                    firstly {
-                        reducer.reduce(store: self, action: action, completion: completion)
-                    }.then { actionResult -> Void in
-                        fulfill(actionResult)
-                    }.catch { error in
-                        debugPrint("Error: \(error)")
-                        reject(error)
-                    }
-                }
-                else {
-                    pendingStoreActions.append((condition, action, completion))
-                    fulfill(nil)
-                }
+            firstly {
+                reducer.reduce(store: self, action: action, completion: completion)
+            }.then { actionResult -> Void in
+                fulfill(actionResult)
+            }.catch { error in
+                debugPrint("Error: \(error)")
+                reject(error)
             }
-            else {
-                
-                firstly {
-                    reducer.reduce(store: self, action: action, completion: completion)
-                }.then { actionResult -> Void in
-                    fulfill(actionResult)
-                }.catch { error in
-                    debugPrint("Error: \(error)")
-                    reject(error)
-                }
-            }
-        }
-    }
-    
-    // validate if any pendingStoreClosures can be run and execute
-    // them if they are.
-    public func executeExecutablePendingStoreClosures() -> Promise<Void> {
-        
-        return Promise<Void> { fulfill, reject in
-            
-            var executedPendingStoreClosuresIndexes = [Int]()
-            var lastPromise: Promise<ActionResult?>? = nil
-            
-            for (index, (condition, closure, completion)) in pendingStoreClosures.enumerated() {
-                
-                if condition(self) {
-                    
-                    if let _lastPromise = lastPromise {
-                        
-                        _lastPromise.then { (_) -> Promise<ActionResult?> in
-                            
-                            lastPromise = self.dispatch(closure: closure, condition: nil, completion: completion)
-                            executedPendingStoreClosuresIndexes.append(index)
-                            return lastPromise!
-                        }.catch { error -> Void in
-                            debugPrint("Error: \(error)")
-                            reject(error)
-                        }
-                    }
-                    else {
-                        lastPromise = self.dispatch(closure: closure, condition: nil, completion: completion)
-                        executedPendingStoreClosuresIndexes.append(index)
-                    }
-                }
-            }
-            
-            if let lastPromise = lastPromise {
-                
-                // FIXME: need to understand why we an not put this in the then...
-                // we don't really care but the fact is we like to understand why things
-                // are the way they are.
-                self.deleteExecutedPendingStoreClosures(indexes: executedPendingStoreClosuresIndexes)
-                
-                lastPromise.then { (_) -> Void in
-                    fulfill(())
-                }.catch { error in
-                    debugPrint("Error: \(error)")
-                    reject(error)
-                }
-            } else {
-                
-                assert(executedPendingStoreClosuresIndexes.count == 0)
-                fulfill(())
-            }
-        }
-    }
-    
-    public func executeExecutablePendingStoreActions() -> Promise<Void> {
-        
-        return Promise<Void> { fulfill, reject in
-            
-            var executedPendingStoreActionsIndexes = [Int]()
-            var lastPromise: Promise<ActionResult?>? = nil
-            
-            for (index, (condition, action, completion)) in pendingStoreActions.enumerated() {
-                
-                if condition(self) {
-                    
-                    if let _lastPromise = lastPromise {
-                        
-                        _lastPromise.then { _  -> Promise<ActionResult?> in
-                            
-                            lastPromise = self.dispatch(action: action, condition: nil, completion: completion)
-                            executedPendingStoreActionsIndexes.append(index)
-                            return lastPromise!
-                        }.catch { error in
-                            debugPrint("Error: \(error)")
-                            reject(error)
-                        }
-                    }
-                    else {
-                        lastPromise = self.dispatch(action: action, condition: nil, completion: completion)
-                        executedPendingStoreActionsIndexes.append(index)
-                    }
-                }
-            }
-            
-            if let lastPromise = lastPromise {
-                
-                self.deleteExecutedPendingStoreActions(indexes: executedPendingStoreActionsIndexes)
-                
-                lastPromise.then {_ -> Void in
-                    fulfill(())
-                }.catch { error in
-                    debugPrint("Error: \(error)")
-                    reject(error)
-                }
-            } else {
-                
-                assert(executedPendingStoreActionsIndexes.count == 0)
-                fulfill(())
-            }
-        }
-    }
-    
-    private func deleteExecutedPendingStoreClosures(indexes: [Int]) {
-        
-        var _reversedIndexes: [Int] = indexes.reversed()
-        var removedValues = 0
-        
-        while !_reversedIndexes.isEmpty {
-            
-            let lastIndex = _reversedIndexes.first!
-            pendingStoreClosures.remove(at: lastIndex - removedValues)
-            _reversedIndexes.remove(at: lastIndex - removedValues)
-            removedValues += 1
-        }
-    }
-    
-    private func deleteExecutedPendingStoreActions(indexes: [Int]) {
-        
-        var _reversedIndexes: [Int] = indexes.reversed()
-        var removedValues = 0
-        
-        while !_reversedIndexes.isEmpty {
-            
-            let lastIndex = _reversedIndexes.first!
-            pendingStoreActions.remove(at: lastIndex - removedValues)
-            _reversedIndexes.remove(at: lastIndex - removedValues)
-            removedValues += 1
         }
     }
 }
